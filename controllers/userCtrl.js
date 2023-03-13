d865bb8751f6a2ea4b365fbe4ed1ec10a2929bb4
fazrin aisha <fazrinmaanu1614@gmail.com>
2023-03-13 20:49:07 +0530

    userControl

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,3 +2,4 @@ const loginController = () => {};
 const registerController = () => {};
 
 module.exports = { loginController, registerController };
+module.exports1 = { loginController, registerController };
